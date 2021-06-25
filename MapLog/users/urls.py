e57530327c0e97af3149fe5a2e9d696c317c744c
Soo Jeong <totnwjd530@gmail.com>
2021-06-26 00:23:08 +0900

    url/views/login창 수정

--- MapLog/users/urls.py
+++ MapLog/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views as login_views
+from . import views as signup_views
 
 app_name = "users"
 
 urlpatterns = [
     path("", login_views.login_page, name="login_page"),
+    path("signup/", signup_views.signup_page, name="signup_page")
 ]
--- MapLog/users/views.py
+++ MapLog/users/views.py
@@ -1,6 +1,8 @@
 import requests
 from django.shortcuts import render
 
-
 def login_page(request):
     return render(request, "users/login.html")
+
+def signup_page(request):
+    return render(request, "users/signup.html")

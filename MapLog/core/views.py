45f48eb12910b2d74e7b1dba3560f8755947d3c1
Somin Park <itsmechloe@naver.com>
2021-07-02 23:23:56 +0900

    Feat: Add support page

--- MapLog/core/views.py
+++ MapLog/core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def about_us(request):
     return render(request, "menu/about_us.html")
+
+def support(request):
+    return render(request, "menu/support.html")

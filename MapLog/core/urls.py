3562e1669d2eae1f3882ce144334d87cb90405fd
Somin Park <itsmechloe@naver.com>
2021-07-03 05:00:37 +0900

    Feat: Add main page

--- MapLog/core/urls.py
+++ MapLog/core/urls.py
@@ -4,6 +4,7 @@ from . import views as core_views
 app_name = "core"
 
 urlpatterns = [
+    path("", core_views.main_page, name="main_page"),
     path("about-us/", core_views.about_us, name="about_us"),
     path("support/", core_views.support, name="support"),
 ]
